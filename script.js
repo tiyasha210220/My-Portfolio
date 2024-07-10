e8449ab692f8acec5a15b68dce5c979a0ab81bfa
Tiyasha Sharma <98461831+tiyasha210220@users.noreply.github.com>
2024-07-10 09:38:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// --------------------------------------------------------------------------------certificate-swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+   
+  });
+
+// -----------------------------------------------------------------------------toggle-menu-buttton
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
